f95f18cdd32d404103f78448703e0b30eaeb2dce
juhyeonl <juhyeonl@student.hive.fi>
2025-09-09 15:42:10 +0300

    add

--- philo/srcs/philo_routine.c
+++ philo/srcs/philo_routine.c
@@ -6,7 +6,7 @@
 /*   By: JuHyeon <JuHyeon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 11:19:03 by JuHyeon           #+#    #+#             */
-/*   Updated: 2025/09/09 03:06:18 by JuHyeon          ###   ########.fr       */
+/*   Updated: 2025/09/09 15:41:38 by JuHyeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static void	philo_eat_sleep_think(t_philo *philo)
 	t_info	*info;
 
 	info = philo->info;
-	usleep(500);
+	usleep(200);
 	if (philo->id % 2 == 0)
 		take_forks_even(philo);
 	else
@@ -50,33 +50,6 @@ static void	philo_eat_sleep_think(t_philo *philo)
 	if (info->num_philo % 2)
 		usleep(500);
 }
-
-// static void	philo_eat_sleep_think(t_philo *philo)
-// {
-// 	t_info	*info;
-
-// 	info = philo->info;
-    
-// 	// 생각하는 상태를 출력한 후, 잠시 대기하여 포크 경쟁을 완화합니다.
-// 	print_status(philo, "is thinking");
-// 	usleep(200); // 100ms 지연 추가
-// 	if (philo->id % 2 == 0)
-// 		take_forks_even(philo);
-// 	else
-// 		take_forks_odd(philo);
-	
-// 	if (simulation_finished(info))
-// 	{
-// 		put_forks(philo);
-// 		return ;
-// 	}
-// 	eat(philo);
-// 	put_forks(philo);
-// 	if (simulation_finished(info))
-// 		return ;
-// 	print_status(philo, "is sleeping");
-// 	my_usleep(info->tts, info);
-// }
 
 void	*philo_routine(void *arg)
 {
